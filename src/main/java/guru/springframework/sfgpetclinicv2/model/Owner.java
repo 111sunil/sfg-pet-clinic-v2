b9551fd0a7f23f0e65e8407c7e68a2357b6e69e1
111sunil <SuniLch@111>
2020-12-23 11:29:47 +0530

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinicv2/model/Owner.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfgpetclinicv2.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/guru/springframework/sfgpetclinicv2/model/Vet.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfgpetclinicv2.model;
+
+public class Vet extends Person{
+
+}
